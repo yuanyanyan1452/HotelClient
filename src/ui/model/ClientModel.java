52697995cb6cfac9a8048dc68168b8c74c08cd85
xyc <464243149@qq.com>
2016-12-06 23:30:57 +0800

    v2.4
    实现hotelstrategy相关操作；
    配置manager界面完成；
    初步实现manager的增删改客户功能；
    重新添加vipinfo类，因为用到了。。。

--- src/ui/model/ClientModel.java
+++ src/ui/model/ClientModel.java
@@ -1,5 +1,96 @@
 package ui.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import objects.VIPInfo;
+import objects.VIPInfo.VIPType;
+
 public class ClientModel {
+	private final SimpleStringProperty clientid;
+	private final SimpleStringProperty clientName;
+	private final SimpleStringProperty contact;
+	private final SimpleStringProperty vipinfo;
+	private final SimpleStringProperty credit;
+
+	public ClientModel(int clientid, String clientName, String contact, VIPInfo info, int credit) {
+		this.clientid = new SimpleStringProperty(clientid + "");
+		this.clientName = new SimpleStringProperty(clientName);
+		this.contact = new SimpleStringProperty(contact);
+		String vipinfo = "非会员";
+		if (info != null) {
+			if (info.getType().equals(VIPType.NORMAL)) {
+				vipinfo = "普通会员";
+			} else {
+				vipinfo = "企业会员";
+			}
+			vipinfo += "//";
+			vipinfo += info.getInfo();
+		}
+		this.vipinfo = new SimpleStringProperty(vipinfo);
+		this.credit = new SimpleStringProperty(credit + "");
+	}
+
+	public String getID() {
+		return clientid.get();
+	}
+
+	public void setID(int clientid) {
+		this.clientid.set(clientid + "");
+	}
+
+	public SimpleStringProperty iDProperty() {
+		return clientid;
+	}
+
+	public String getName() {
+		return clientName.get();
+	}
+
+	public void setName(String name) {
+		this.clientName.set(name);
+	}
+
+	public SimpleStringProperty nameProperty() {
+		return clientName;
+	}
+
+	public String getContact() {
+		return contact.get();
+	}
+
+	public void setContact(String contact) {
+		this.contact.set(contact);
+	}
+
+	public SimpleStringProperty contactProperty() {
+		return contact;
+	}
+
+	public String getVIPinfo() {
+		return vipinfo.get();
+	}
+
+	public void setVIPinfo(VIPInfo info) {
+		if (info == null) {
+			this.vipinfo.set("非会员");
+		}
+		String type = info.getType() == VIPType.NORMAL ? "普通会员" : "企业会员";
+		this.vipinfo.set(type + "//" + info.getInfo());
+	}
+
+	public SimpleStringProperty viProperty() {
+		return vipinfo;
+	}
+
+	public String getCredit() {
+		return credit.get();
+	}
+
+	public void setCredit(int credit) {
+		this.credit.set(credit + "");
+	}
+
+	public SimpleStringProperty creditProperty() {
+		return credit;
+	}
 
 }
--- src/ui/view/manager/ManagerOverviewController.java
+++ src/ui/view/manager/ManagerOverviewController.java
@@ -1,16 +1,32 @@
 package ui.view.manager;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.*;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import objects.VIPInfo;
+import objects.VIPInfo.VIPType;
 import ui.model.*;
 import ui.view.Main;
 
 public class ManagerOverviewController implements Initializable {
 	private Main main;
+	
+	/*
+	 * 此controller持有系统内所有client，hotel，hotelworker和market的列表
+	 */
+	private ObservableList<ClientModel> clientList = FXCollections.observableArrayList();
+	private ArrayList<Integer> clientIDList=new ArrayList<Integer>();
+	private ClientModel currentClientModel;
+	private ObservableList<HotelModel> hotelList = FXCollections.observableArrayList();
+	private ObservableList<HotelWorkerModel> workerList = FXCollections.observableArrayList();
+	private ObservableList<MarketModel> marketList = FXCollections.observableArrayList();
 	
 	@FXML
 	private Label clientNumLabel;
@@ -21,14 +37,12 @@ public class ManagerOverviewController implements Initializable {
 	@FXML
 	private Label orderNumLabel;
 	
-	@FXML
-	private TextField nameTextField;
-	
-	@FXML
-	private Button searchButton;
 	
 	@FXML
 	private TableView<ClientModel> clientTable;
+	
+	@FXML
+	private TableColumn<ClientModel, String> clientIDColumn;
 	
 	@FXML
 	private TableColumn<ClientModel, String> clientNameColumn;
@@ -41,18 +55,54 @@ public class ManagerOverviewController implements Initializable {
 	
 	@FXML
 	private TableColumn<ClientModel, String> clientCreditColumn;
+
+	@FXML
+	private TextField addClientNameTextField;
 	
 	@FXML
-	private Button clientAddButton;
+	private TextField 	addClientContactTextField;
 	
 	@FXML
-	private Button clientDeleteButton;
+	private TextField addClientVIPTextField;
 	
 	@FXML
-	private Button clientUpdateButton;
+	private TextField addClientCreditTextField;
+	
+	@FXML
+	private TextField updateClientIDTextField;
+	
+	@FXML
+	private TextField updateClientNameTextField;
+	
+	@FXML
+	private TextField 	updateClientContactTextField;
+	
+	@FXML
+	private TextField updateClientVIPTextField;
+	
+	@FXML
+	private TextField updateClientCreditTextField;
+	
+	@FXML
+	private TextField deleteClientIDTextField;
+	
+	@FXML
+	private Label deleteClientNameLabel;
+	
+	@FXML
+	private Label deleteClientContactLabel;
+	
+	@FXML
+	private Label deleteClientVIPLabel;
+	
+	@FXML
+	private Label deleteClientCreditLabel;
 	
 	@FXML
 	private TableView<HotelModel> hotelTable;
+	
+	@FXML
+	private TableColumn<HotelModel, String> hotelIDColumn;
 	
 	@FXML
 	private TableColumn<HotelModel, String> hotelNameColumn;
@@ -68,15 +118,57 @@ public class ManagerOverviewController implements Initializable {
 	
 	@FXML
 	private TableColumn<HotelModel, String> hotelScoreColumn;
+
+	@FXML
+	private TextField addHotelNameTextField;
 	
 	@FXML
-	private Button hotelAddButton;
+	private TextField 	addHotelBusinessAddressTextField;
 	
 	@FXML
-	private Button hotelDeleteButton;
+	private TextField addHotelAddressTextField;
 	
 	@FXML
-	private Button hotelUpdateButton;
+	private TextField addHotelStarTextField;
+	
+	@FXML
+	private TextField addHotelScoreTextField;
+	
+	@FXML
+	private TextField updateHotelIDTextField;
+	
+	@FXML
+	private TextField updateHotelNameTextField;
+	
+	@FXML
+	private TextField 	updateHotelBusinessAddressTextField;
+	
+	@FXML
+	private TextField updateHotelAddressTextField;
+	
+	@FXML
+	private TextField updateHotelStarTextField;
+	
+	@FXML
+	private TextField updateHotelScoreTextField;
+	
+	@FXML
+	private TextField deleteHotelIDTextField;
+	
+	@FXML
+	private Label deleteHotelNameLabel;
+	
+	@FXML
+	private Label deleteHotelBusinessAddressLabel;
+	
+	@FXML
+	private Label deleteHotelAddressLabel;
+	
+	@FXML
+	private Label deleteHotelStarLabel;
+
+	@FXML
+	private Label deleteHotelScoreLabel;
 	
 	@FXML
 	private TableView<HotelWorkerModel> hotelWorkerTable;
@@ -91,17 +183,37 @@ public class ManagerOverviewController implements Initializable {
 	private TableColumn<HotelWorkerModel, String> workerContactColumn;
 	
 	@FXML
-	private Button workerAddButton;
+	private TextField addWorkerIDTextField;
 	
 	@FXML
-	private Button workerDeleteButton;
+	private TextField addWorkerNameTextField;
 	
 	@FXML
-	private Button workerUpdateButton;
+	private TextField addWorkerContactTextField;
+	
+	@FXML
+	private TextField updateWorkerIDTextField;
+	
+	@FXML
+	private TextField updateWorkerNameTextField;
+	
+	@FXML
+	private TextField updateWorkerContactTextField;
+	
+	@FXML
+	private TextField deleteWorkerIDTextField;
+	
+	@FXML
+	private Label deleteWorkerNameLabel;
+	
+	@FXML
+	private Label deleteWorkerContactLabel;
 	
 	@FXML
 	private TableView<MarketModel> marketTable;
 	
+	@FXML
+	private TableColumn<MarketModel, String> marketIDColumn;
 	@FXML
 	private TableColumn<MarketModel, String> marketNameColumn;
 	
@@ -109,13 +221,199 @@ public class ManagerOverviewController implements Initializable {
 	private TableColumn<MarketModel, String> marketContactColumn;
 	
 	@FXML
-	private Button marketAddButton;
+	private TextField addMarketNameTextField;
 	
 	@FXML
-	private Button marketDeleteButton;
+	private TextField addMarketContactTextField;
 	
 	@FXML
-	private Button marketUpdateButton;
+	private TextField updateMarketIDTextField;
+	
+	@FXML
+	private TextField updateMarketNameTextField;
+	
+	@FXML
+	private TextField updateMarketContactTextField;
+	
+	@FXML
+	private TextField deleteMarketIDTextField;
+	
+	@FXML
+	private Label deleteMarketNameLabel;
+	
+	@FXML
+	private Label deleteMarketContactLabel;
+	
+	
+	@FXML
+	private void addClient(){
+		int id = clientList.size()+1;
+		String name = addClientNameTextField.getText();
+		String contact = addClientContactTextField.getText();
+		VIPInfo info= null;
+		if(!addClientVIPTextField.getText().equals("")){
+			String[] vipinfo = addClientVIPTextField.getText().split("//");
+			info = new VIPInfo(vipinfo[0].equals("普通会员")? VIPType.NORMAL:VIPType.Enterprise, vipinfo[1]);
+		}
+		int credit = Integer.parseInt(addClientCreditTextField.getText());
+		
+		ClientModel client = new ClientModel(id, name, contact, info, credit);
+		clientTable.getItems().add(client);
+	}
+	
+	@FXML
+	private void updateSearchClient(){
+		String id = updateClientIDTextField.getText();
+		boolean isSuccess = false;
+		
+		for (ClientModel client : clientList) {
+			if (client.getID().equals(id)) {
+				currentClientModel = client;
+				updateClientNameTextField.setText(client.getName());
+				updateClientContactTextField.setText(client.getContact());
+				updateClientVIPTextField.setText(client.getVIPinfo());
+				updateClientCreditTextField.setText(client.getCredit());
+				
+				isSuccess = true;
+			}
+		}
+		if(!isSuccess){
+			updateClientNameTextField.setText("");
+			updateClientContactTextField.setText("");
+			updateClientVIPTextField.setText("");
+			updateClientCreditTextField.setText("");
+		
+		}
+	}
+	
+	@FXML
+	private void updateClient(){
+		clientList.remove(currentClientModel);
+		
+		currentClientModel.setName(updateClientNameTextField.getText());
+		currentClientModel.setContact(updateClientContactTextField.getText());
+		String tempvipinfo = updateClientVIPTextField.getText();
+		VIPInfo info = null;
+		if(!tempvipinfo.equals("")){
+			String[] tempInfos = tempvipinfo.split("//");
+			info = new VIPInfo(tempInfos[0]=="普通会员"? VIPType.NORMAL:VIPType.Enterprise,tempInfos[1]);
+		}
+		currentClientModel.setVIPinfo(info);
+		currentClientModel.setCredit(Integer.parseInt(updateClientCreditTextField.getText()));
+		
+		clientList.add(currentClientModel);
+	}
+	
+	@FXML
+	private void deleteSearchClient(){
+		String id = deleteClientIDTextField.getText();
+		boolean isSuccess = false;
+		
+		for (ClientModel client : clientList) {
+			if (client.getID().equals(id)) {
+				currentClientModel = client;
+				deleteClientNameLabel.setText(client.getName());
+				deleteClientContactLabel.setText(client.getContact());
+				deleteClientVIPLabel.setText(client.getVIPinfo());
+				deleteClientCreditLabel.setText(client.getCredit());
+				
+				isSuccess = true;
+			}
+		}
+		if(!isSuccess){
+			deleteClientNameLabel.setText("");
+			deleteClientContactLabel.setText("");
+			deleteClientVIPLabel.setText("");
+			deleteClientCreditLabel.setText("");
+		
+		}
+	}
+	
+	@FXML
+	private void deleteClient(){
+		if(clientList.contains(currentClientModel)){
+			clientList.remove(currentClientModel);
+			clientIDList.remove(clientIDList.indexOf(currentClientModel.getID())+1);
+		}
+		else{
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("不存在该客户！");
+			alert.setHeaderText("出错了！");
+			alert.show();
+		}
+	}
+	
+	@FXML
+	private void addHotel(){
+		//TODO
+	}
+	
+	@FXML
+	private void updateHotel(){
+		//TODO
+	}
+	
+	@FXML
+	private void updateSearchHotel(){
+		//TODO
+	}
+	
+	@FXML
+	private void deleteSearchHotel(){
+		
+	}
+	
+	@FXML
+	private void deleteHotel(){
+		//TODO
+	}
+	
+	@FXML
+	private void addHotelWorker(){
+		//TODO
+	}
+	
+	@FXML
+	private void updateSearchHotelWorker(){
+		
+	}
+	@FXML
+	private void updateHotelWorker(){
+		//TODO
+	}
+	
+	@FXML
+	private void deleteSearchHotelWorker(){
+		
+	}
+	
+	@FXML
+	private void deleteHotelWorker(){
+		//TODO
+	}
+	
+	@FXML
+	private void addMarket(){
+		//TODO
+	}
+	
+	@FXML
+	private void updateSearchMarket(){
+		
+	}
+	@FXML
+	private void updateMarket(){
+		//TODO
+	}
+	
+	@FXML
+	private void deleteSearchMarket(){
+		
+	}
+	@FXML
+	private void deleteMarket(){
+		//TODO
+	}
 	
 	public ManagerOverviewController() {
 		
@@ -127,5 +425,19 @@ public class ManagerOverviewController implements Initializable {
 
 	public void setMain(Main main) {
 		this.main = main;
+		
+		clientList.add(new ClientModel(1, "张三", "18888888888", new VIPInfo(VIPType.NORMAL, "1999/01/01"), 0));
+		clientList.add(new ClientModel(2, "李四", "13333333333",null, 0));
+		
+		for(ClientModel clientModel:clientList){
+			clientIDList.add(Integer.parseInt(clientModel.getID()));
+		}
+		clientIDColumn.setCellValueFactory(celldata->celldata.getValue().iDProperty());
+		clientNameColumn.setCellValueFactory(celldata->celldata.getValue().nameProperty());
+		clientContactColumn.setCellValueFactory(celldata->celldata.getValue().contactProperty());
+		clientVipColumn.setCellValueFactory(celldata->celldata.getValue().viProperty());
+		clientCreditColumn.setCellValueFactory(celldata->celldata.getValue().creditProperty());
+		
+		clientTable.setItems(clientList);
 	}
 }
